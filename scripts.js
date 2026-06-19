1d5804b060576b032efced17dd040446dc29ac91
Tales Lopes <tales.tette70@gmail.com>
2026-06-19 22:25:36 +0100

    initial commit: created first HTML, CSS, JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+        function aumentarImagem() {
+            const img = document.getElementById("minhaImagem");
+            img.width += 20;
+        }
+
+        function diminuirImagem() {
+            const img = document.getElementById("minhaImagem");
+            img.width -= 20;
+        }
+    
